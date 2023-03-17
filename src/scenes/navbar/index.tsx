d02d0efd435b14e2aa93c1b6e1c7ffad8d9aabf2
Bralex-svg <alexadam269@gmail.com>
2023-03-17 14:28:25 -0700

    updated

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -11,7 +11,11 @@ const Navbar = (props: NavbarProps) => {
       <div className={` ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16 `}>
+            {/* LEFT SIDE */}
             <img src={Logo} alt="logo" />
+
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full `}></div>
           </div>
         </div>
       </div>
